f52342ca60124500305851c97070eb5447a1b9b5
Kirill Shatskiy <shaackiy@gmail.com>
2018-05-09 14:22:39 +0300

    Update examples.

--- src/examples/index.js
+++ src/examples/index.js
@@ -1,15 +1,51 @@
 import './../Gridmap/Gridmap';
-import data from './data.geojson';
+import data from './data-latlong.geojson';
 
 ymaps.ready(() => {
     ymaps.modules.require(['Gridmap'], (Gridmap) => {
-        const el = document.getElementById('map');
-        const myMap = new ymaps.Map(el, {
-            center: [55.76, 37.64],
-            zoom: 10,
-            controls: ['zoomControl', 'typeSelector', 'fullscreenControl']
-        });
+        const hexagonMap = new ymaps.Map(
+            document.getElementById('hexagonMap'),
+            {
+                center: [55.76, 37.64],
+                zoom: 10,
+                controls: ['zoomControl', 'typeSelector', 'fullscreenControl']
+            });
         // eslint-disable-next-line no-unused-vars
-        const gridmap = new Gridmap(el, myMap, data);
+        const hexagonGridmap = new Gridmap({
+            map: hexagonMap,
+            points: data,
+            zoom: 10,
+            grid: {
+                type: 'hexagon',
+                params: {
+                    bigRadius: 20
+                }
+            }
+        });
+
+        const squareMap = new ymaps.Map(
+            document.getElementById('squareMap'),
+            {
+                center: [55.76, 37.64],
+                zoom: 10,
+                controls: ['zoomControl', 'typeSelector', 'fullscreenControl']
+            });
+
+        // eslint-disable-next-line no-unused-vars
+        const squareGridmap = new Gridmap({
+            map: squareMap,
+            points: data,
+            zoom: 10,
+            grid: {
+                type: 'square',
+                params: {
+                    sideLength: 20
+                },
+                bounds: {
+                    leftBottom: [55.58709262753493, 37.33818169628907],
+                    rightTop: [55.89311385727207, 37.848359308593736]
+                }
+            }
+        });
     });
 });
