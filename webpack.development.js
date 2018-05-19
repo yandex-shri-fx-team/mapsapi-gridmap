6125cc0dc18946a2e122cec493c9ceb9f8e157c0
vit134 <vit134@mail.ru>
2018-05-19 16:15:36 +0300

    build: add style loader to build

--- webpack.development.js
+++ webpack.development.js
@@ -26,6 +26,17 @@ module.exports = {
                         loader: 'html-loader'
                     }
                 ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
             }
         ]
     },
