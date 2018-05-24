5fa25f1a350c78ac1e43b1832291c0d4d362fbe5
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-05-24 12:31:34 +0300

    feat(examples): update

---
+++ examples/hexagonGrid/index.js
@@ -0,0 +1,35 @@
+import '../../src/Gridmap';
+import data from '../data/bikeparking-moscow.geojson';
+
+ymaps.ready(() => {
+    ymaps.modules.require(['Gridmap'], (Gridmap) => {
+        // eslint-disable-next-line no-unused-vars
+        const myMap = new ymaps.Map('map', {
+            center: [37.64, 55.76],
+            zoom: 10,
+            controls: ['zoomControl', 'typeSelector']
+        });
+
+        const hexagonGridmap = new Gridmap(data, {
+            zoom: 10,
+            grid: {
+                type: 'hexagon',
+                params: {
+                    bigRadius: 20
+                },
+                bounds: {
+                    leftBottom: myMap.getBounds()[0],
+                    rightTop: myMap.getBounds()[1]
+                }
+            },
+            colorScheme: 'cdom',
+            colorRanges: 10,
+            colorOpacity: 0.8,
+            strokeColor: '#fff',
+            strokeWidth: 1.5,
+            filterEmptyPolygons: true
+        });
+
+        hexagonGridmap.setMap(myMap);
+    });
+});
